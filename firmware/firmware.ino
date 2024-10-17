0dd8733d46eab27c975bdd4dedf4a22037c20d46
Matt Peachey <peacheym@dal.ca>
2024-10-17 13:28:26 -0300

    Push latest firmware

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,14 +1,24 @@
+#include <Adafruit_NeoPixel.h>
+
 #include <WiFi.h>
 #include <mapper.h>
 #include <Adafruit_MCP4728.h>
 #include <Wire.h>
 
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+
+Adafruit_NeoPixel pixels(1, 4, NEO_RGB + NEO_KHZ800);
+#define DELAYVAL 500
+
+
 // For disabling power saving
 #include "esp_wifi.h"
 Adafruit_MCP4728 mcp;
 
-const char* ssid = "*************";
-const char* password = "*************";
+const char* ssid = "GemLab";
+const char* password = "gemlabdal";
 
 mpr_dev dev = 0;
 mpr_sig inputSignal0 = 0;
@@ -20,6 +30,17 @@ float receivedValue = 0;
 int DEBUG = 1;
 
 void setup() {  
+
+  int led_status_ind = 0;
+
+  #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
+    clock_prescale_set(clock_div_1);
+  #endif
+
+  pixels.begin();
+  pixels.setPixelColor(0, pixels.Color(155, 0, 0));
+  pixels.show();
+
   WiFi.begin(ssid, password);
   Serial.begin(115200);
   
@@ -30,8 +51,24 @@ void setup() {
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
+    if (led_status_ind==0){
+      pixels.setPixelColor(0, pixels.Color(0,0,155));
+      led_status_ind=1;
+    }
+    else{
+      pixels.setPixelColor(0, pixels.Color(155, 0, 0));
+      led_status_ind=0;
+    }
+    pixels.show();
+
   }
   Serial.println("Connected to WiFi");
+
+//  pixels.setPixelColor(0, pixels.Color(0, 155, 0));
+//  pixels.show();
+//  pixels.setPixelColor(0, pixels.Color(0, 0, 155));
+//  pixels.show();
+
 
   if(!mcp.begin(0x64)){
     Serial.println("Failed to find MCP4728");
@@ -47,7 +84,7 @@ void setup() {
   float min_pw = 0.0f;
   float max_pw = 4095.0f;  
 
-  dev = mpr_dev_new("CVmapper", 0);
+  dev = mpr_dev_new("Maplet", 0);
 
   Serial.println("Initialized libmapper device");
 
@@ -60,15 +97,38 @@ void setup() {
 
   for(int i=0; i<10; i++){
     mpr_dev_poll(dev, 500);
+
+    if (led_status_ind==0){
+      pixels.setPixelColor(0, pixels.Color(0,155,0));
+      led_status_ind=1;
+    }
+    else{
+      pixels.setPixelColor(0, pixels.Color(0, 0, 155));
+      led_status_ind=0;
+    }
+    pixels.show();
   }
 
   Serial.println("Done Polling");
 
+  pixels.setPixelColor(0, pixels.Color(0, 155, 0));
+  pixels.show();
 
 }
 
 void loop() {  
   mpr_dev_poll(dev, 100);
+
+//  int num_maps = mpr_obj_get_prop_as_int32(inputSignal0, MPR_PROP_NUM_MAPS_IN, NULL);
+//
+//  if(num_maps > 0){
+//    pixels.setPixelColor(0, pixels.Color(0, 155, 67));
+//  }
+//  else{
+//    pixels.setPixelColor(0, pixels.Color(255,0,0));
+//  }
+//  pixels.show();
+
 }
 
 
@@ -77,15 +137,14 @@ void inputSignalHandler(mpr_sig sig, mpr_sig_evt evt, mpr_id inst, int length,
                         mpr_type type, const void* value, mpr_time time) {
     float val = *((float*)value);
 
-    // This is bad programming -- but I soldered the jacks to the wrong DAC pins in V0.1
     if(strcmp(mpr_obj_get_prop_as_str(sig, MPR_PROP_NAME, NULL), "channel1") == 0)
-      mcp.setChannelValue(MCP4728_CHANNEL_C, (int)val);
+      mcp.setChannelValue(MCP4728_CHANNEL_A, (int)val);
     else if (strcmp(mpr_obj_get_prop_as_str(sig, MPR_PROP_NAME, NULL), "channel2") == 0)
-        mcp.setChannelValue(MCP4728_CHANNEL_D, (int)val);
-    else if (strcmp(mpr_obj_get_prop_as_str(sig, MPR_PROP_NAME, NULL), "channel3") == 0)
         mcp.setChannelValue(MCP4728_CHANNEL_B, (int)val);
+    else if (strcmp(mpr_obj_get_prop_as_str(sig, MPR_PROP_NAME, NULL), "channel3") == 0)
+        mcp.setChannelValue(MCP4728_CHANNEL_C, (int)val);
     else if (strcmp(mpr_obj_get_prop_as_str(sig, MPR_PROP_NAME, NULL), "channel4") == 0)
-        mcp.setChannelValue(MCP4728_CHANNEL_A, (int)val);
+        mcp.setChannelValue(MCP4728_CHANNEL_D, (int)val);
     else Serial.println("Error with signal names");
     
 }
